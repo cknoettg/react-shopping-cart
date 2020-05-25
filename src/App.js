dc842257d016e0b657faa3d71e4854d1c8900787
cknoettg <cknoettg@yahoo.com>
2020-05-24 23:39:12 -0400

    Finished Stretch - localStorage and custom hooks

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+//STRETCH
+// Hooks
+import useLocalStorage from './hooks/useLocalStorage';
+
 //Contexts
 import { ProductContext } from "./contexts/ProductContext";
 import {CartContext} from './contexts/CartContext.js';
@@ -13,11 +17,15 @@ import ShoppingCart from './components/ShoppingCart';
 
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const [cart, setCart] = useLocalStorage('cart',[]);
 
 	const addItem = item => {
 		// add the given item to the cart
 		setCart([...cart, item]);
+	};
+
+	const removeItem = id => {
+		setCart(cart.filter(item => item.id !== id));
 	};
 
 	return (
