87dd96082185df3e4e47f1e74515215d3ab13fbf
cknoettg <cknoettg@yahoo.com>
2020-05-24 23:06:24 -0400

    Completed MVP.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+//Contexts
+import { ProductContext } from "./contexts/ProductContext";
+import {CartContext} from './contexts/CartContext.js';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
-
-//Contexts
-import { ProductContext } from "./contexts/ProductContext";
-import {CartContext} from './contexts/CartContext.js';
 
 function App() {
 	const [products] = useState(data);
